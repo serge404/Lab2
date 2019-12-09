007acd787edfbf9e7f0382746f5628d6b87a19db
Serge Selenou <serge.selenou@bison.howard.edu>
2019-12-09 04:58:39 +0000

    3 parts finished

--- signal.c
+++ signal.c
@@ -4,16 +4,22 @@
 #include <signal.h>
 #include <unistd.h>
 
+char* turing = "Turing was right!\n";
+
 void handler(int signum)
 { //signal handler
   printf("Hello World!\n");
-  exit(1); //exit after printing
+  exit(1);
 }
 
 int main(int argc, char * argv[])
 {
-  signal(SIGALRM,handler); //register handler to handle SIGALRM
-  alarm(1); //Schedule a SIGALRM for 1 second
-  while(1); //busy wait for signal to be delivered
-  return 0; //never reached
+  signal(SIGINT,sigint_handler); // Register handler to handle SIGINT.
+  signal(SIGALRM,handler); // Register handler to handle SIGALRM.
+  while(1){
+    alarm(1); // Schedule a SIGALRM for 1 second.
+    sleep(1);
+    printf("Turing was right!\n");
+  }
+	return 0; //never reached
 }
